3761abb5c11842308bc128afd20a7699776c6e0b
Brandon Schmeidler <brandon.bsgstudio@gmail.com>
2021-02-14 07:35:12 -0600

    Added some functions for RtMidiIn class

--- GMSMidi/GMSMidi/src/main.cpp
+++ GMSMidi/GMSMidi/src/main.cpp
@@ -8,6 +8,7 @@ class PointerManager {
 public:
 	int add(T item);
 	int remove(int id);
+	T get(int id);
 private:
 	std::vector<T> m_pointer_list;
 };
@@ -47,6 +48,16 @@ int PointerManager<T>::remove(int id) {
 	return 0;
 }
 
+template <class T>
+T PointerManager<T>::get(int id) {
+	if (m_pointer_list.empty()) return nullptr;
+
+	const int size = m_pointer_list.size();
+	if (id < 0 || id >= size) return nullptr;
+
+	return m_pointer_list[id];
+}
+
 PointerManager<RtMidiIn*> midi_in_ptrs;
 PointerManager<RtMidiOut*> midi_out_ptrs;
 
@@ -56,6 +67,38 @@ GMS_DLL double midi_in_create() {
 
 GMS_DLL double midi_in_destroy(double id) {
 	return midi_in_ptrs.remove(id);
+}
+
+GMS_DLL double midi_in_open_port(double midi_in_id, double port_id) {
+	RtMidiIn* midi_in = midi_in_ptrs.get(midi_in_id);
+	if (midi_in == nullptr) return -1;
+	midi_in->openPort(port_id);
+	return 0;
+}
+
+GMS_DLL double midi_in_close_port(double midi_in_id) {
+	RtMidiIn* midi_in = midi_in_ptrs.get(midi_in_id);
+	if (midi_in == nullptr) return -1;
+	midi_in->closePort();
+	return 0.0;
+}
+
+GMS_DLL double midi_in_is_port_open(double midi_in_id) {
+	RtMidiIn* midi_in = midi_in_ptrs.get(midi_in_id);
+	if (midi_in == nullptr) return -1;
+	return midi_in->isPortOpen() ? GMS_TRUE : GMS_FALSE;
+}
+
+GMS_DLL double midi_in_get_port_count(double midi_in_id) {
+	RtMidiIn* midi_in = midi_in_ptrs.get(midi_in_id);
+	if (midi_in == nullptr) return -1;
+	return midi_in->getPortCount();
+}
+
+GMS_DLL const char* midi_in_get_port_name(double midi_in_id) {
+	RtMidiIn* midi_in = midi_in_ptrs.get(midi_in_id);
+	if (midi_in == nullptr) return "";
+	return midi_in->getPortName().c_str();
 }
 
 GMS_DLL double midi_out_create() {
